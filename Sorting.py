6d7a7b5184a3bc1afd39e4376b4cec0fc060352b
Robert Vukovic <rvukovic@gmail.com>
2017-01-09 13:41:12 +0100

    Sorting

--- Sorting.py
+++ Sorting.py
@@ -1,2 +1,17 @@
 
-print("111111")
+def bub_sort(arr):
+	for i in range(len(arr)):
+		for j in range(i):
+			if arr[i] < arr[j]:
+				arr[i], arr[j] = arr[j], arr[i]
+	return arr
+
+
+ARR = [9, 1, 7, 0, 3, 5, 4, 6, 8, 2]
+
+
+print("Original - ", ARR)
+print("bub_sort - ", bub_sort(ARR))
+
+ARR.sort()
+print("sort     - ", ARR)
