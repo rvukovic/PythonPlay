f5a988c7f19e02ec7909c6c73d9235039b068429
Robert Vukovic <rvukovic@gmail.com>
2017-01-09 13:47:14 +0100

    fixed tabs vs spaces problem

--- Sorting.py
+++ Sorting.py
@@ -1,10 +1,10 @@
 
 def bub_sort(arr):
-	for i in range(len(arr)):
-		for j in range(i):
-			if arr[i] < arr[j]:
-				arr[i], arr[j] = arr[j], arr[i]
-	return arr
+    for i in range(len(arr)):
+        for j in range(i):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 
 ARR = [9, 1, 7, 0, 3, 5, 4, 6, 8, 2]
