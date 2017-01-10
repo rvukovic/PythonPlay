90b1fc6b4a7da2c323b8f6ca305ea8ed5eba3acf
Robert Vukovic <rvukovic@gmail.com>
2017-01-10 13:17:49 +0100

    maximize profit bug fixes

--- MaximizeProfit.py
+++ MaximizeProfit.py
@@ -40,19 +40,72 @@ def maxProfit(price):
     result = profit[n-1]
     return result
 
+# http://keithschwarz.com/interesting/code/?dir=single-sell-profit
+def BruteForceSingleSellProfit(arr):
+    # Store the best possible profit we can make; initially this is 0.
+    bestProfit = 0;
+
+    # Iterate across all pairs and find the best out of all of them.  As a
+    # minor optimization, we don't consider any pair consisting of a single
+    # element twice, since we already know that we get profit 0 from this.
+    for i in range(0, len(arr)):
+        for j in range (i + 1, len(arr)):
+            bestProfit = max(bestProfit, arr[j] - arr[i])
+
+    return bestProfit
+
+
+def DynamicProgrammingSingleSellProfit(arr):
+    # If the array is empty, we cannot make a profit.
+    if len(arr) == 0:
+        return 0
+
+    # Otherwise, keep track of the best possible profit and the lowest value
+    # seen so far.
+    profit = 0
+    cheapest = arr[0]
+
+    # Iterate across the array, updating our answer as we go according to the
+    # above pseudocode.
+    for i in range(1, len(arr)):
+        # Update the minimum value to be the lower of the existing minimum and
+        # the new minimum.
+        cheapest = min(cheapest, arr[i])
+
+        # Update the maximum profit to be the larger of the old profit and the
+        # profit made by buying at the lowest value and selling at the current
+        # price.
+        profit = max(profit, arr[i] - cheapest)
+
+    return profit
+
+
 def findMaxProfit(arr):
     maxPos, maxVal = 0, arr[0]
     minPos, minVal = 0, arr[0]
+    profit = maxVal - minVal
     for i in range(1, len(arr)):
-        if arr[i] < minVal and maxPos >= i:
+        if arr[i] < minVal:
             minPos, minVal = i, arr[i]
-        elif arr[i] > maxVal and maxPos >= minPos:
+
+        tmpProfit = arr[i] - minVal
+        if profit < tmpProfit:
             maxPos, maxVal = i, arr[i]
+            profit = tmpProfit
 
-    return maxVal-minVal, minPos+1, maxPos+1
+    return profit, minPos+1, maxPos+1
+
+ARR1 = [-1, 0, 3, 6, 5, 2, -10]
+ARR2 = [5, 10, 4, 6, 12]
+ARR3 = [-10, 4, 3, 10, 30]
+ARR4 = [15, 10, 4, 6, 12, -10]
 
 
-ARR = [ -1, 0 ,3 , 6, 5, 2, -10]
+ARR_N = [ARR1, ARR2, ARR3, ARR4]
 
-print(maxProfit(ARR))
-print(findMaxProfit(ARR))
+for k in ARR_N:
+    print("My    - ", findMaxProfit(k)) 
+    #print("Max   - ", maxProfit(i)) # incorrect
+    print("Dyn   - ", DynamicProgrammingSingleSellProfit(k))
+    print("Brute - ", BruteForceSingleSellProfit(k))
+    print("------")
